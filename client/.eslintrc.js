b33c7125ca4a882357c84891426d67a2af1f7887
Digtyarenk0 <uhmeplay811@gmail.com>
2022-10-29 12:43:53 +0300

    add import order

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -3,12 +3,33 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['eslint:recommended', 'plugin:prettier/recommended', 'prettier', 'plugin:react/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+    'plugin:react/recommended',
+  ],
   overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['react', 'prettier'],
-  rules: {},
+  plugins: ['react', 'prettier', 'import'],
+  rules: {
+    'no-debugger': 'warn',
+    'import/no-unresolved': 'off',
+    'import/no-unresolved': 'off',
+    'import/order': [
+      'error',
+      {
+        alphabetize: {
+          order: 'asc',
+        },
+        'newlines-between': 'always',
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+      },
+    ],
+  },
 };
